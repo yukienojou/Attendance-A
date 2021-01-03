2e8f1fec0e1f0e41adbad68b104772b8e668e150
yukie nameki <y.nameki1205@ymobile.ne.jp>
2021-01-03 12:09:11 +0000

    8.5.2削除未

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,13 @@
 class UsersController < ApplicationController
-  before_action :set_user, only: [:show, :edit, :update]
-  before_action :logged_in_user, only: [:show, :edit, :update]
+  before_action :set_user, only: [:show, :edit, :update, :destroy]
+  before_action :logged_in_user, only: [:index, :show, :edit, :update, :destroy]
   before_action :correct_user, only: [:edit, :update]
+  before_action :admin_user, only: :destroy
+  def index
+    @users = User.paginate(page: params[:page])
+  end  
   
   def show
-    @user = User.find(params[:id])
     
   end
   
@@ -24,11 +27,10 @@ class UsersController < ApplicationController
   end
   
   def edit
-    @user = User.find(params[:id])
+    
   end
   
   def update
-    @user = User.find(params[:id])
     if @user.update_attributes(user_params)
       flash[:success] = "ユーザー情報を更新しました。"
       redirect_to @user
@@ -37,6 +39,12 @@ class UsersController < ApplicationController
     end
   end  
   
+  def destroy
+    @user.destroy
+    flash[:success] = "#{@user.name}のデータを削除しました。"
+    redirect_to users_url
+  end
+  
   private
   
     def user_params
@@ -44,11 +52,12 @@ class UsersController < ApplicationController
     end
     
     def set_user
-      @user = User.fnd(params[:id])
+      @user = User.find(params[:id])
     end
     
     def logged_in_user
       unless logged_in?
+        store_location
         flash[:danger] = "ログインしてください。"
         redirect_to login_url
       end
@@ -57,6 +66,10 @@ class UsersController < ApplicationController
     def correct_user
    
       redirect_to(root_url) unless current_user?(@user)
+    end
+    
+    def admin_user
+      redirect_to root_url unless current_user.admin?
     end
   
 end
