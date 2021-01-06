e30a551efbbd6c43c3318695a4a3970de09d0c8b
yukie nameki <y.nameki1205@ymobile.ne.jp>
2021-01-06 12:21:27 +0000

    9.3.1モーダルウィンドウ途中

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 
 Rails.application.routes.draw do
-  get 'sessions/new'
 
   root 'static_pages#top'
   get '/signup', to: 'users#new'
@@ -9,6 +8,10 @@ Rails.application.routes.draw do
   post   '/login', to: 'sessions#create'
   delete '/logout', to: 'sessions#destroy'
   
-  resources :users
-  
+  resources :users do
+    member do
+      get 'edit_basic_info'
+      patch 'update_basic_info'
+    end
+  end
 end
