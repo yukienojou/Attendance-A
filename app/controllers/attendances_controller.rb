d51129f8952f5879e77a485bdf913e299747dd4e
yukie nameki <y.nameki1205@ymobile.ne.jp>
2021-01-13 12:08:10 +0000

    11.6debugger

--- app/controllers/attendances_controller.rb
+++ app/controllers/attendances_controller.rb
@@ -1,6 +1,6 @@
 class AttendancesController < ApplicationController
   
-  before_action :set_user, only: :edit_one_month
+  before_action :set_user, only: [:edit_one_month, :update_one_month]
   before_action :logged_in_user, only: [:update, :edit_one_month]
   before_action :set_one_month, only: :edit_one_month
   
@@ -28,4 +28,9 @@ class AttendancesController < ApplicationController
   
   def edit_one_month
   end
+  
+  def update_one_month
+  
+  end
+
 end
