849e4ab36b172cdd94a4e8bc91a539cf70a5a320
yukie nameki <y.nameki1205@ymobile.ne.jp>
2021-01-15 12:13:47 +0000

    11.3.3 from end

--- app/controllers/attendances_controller.rb
+++ app/controllers/attendances_controller.rb
@@ -30,6 +30,18 @@ class AttendancesController < ApplicationController
   end
   
   def update_one_month
+    ActiveRecord::Base.transaction do
+      attendances_params.each do |id, item| 
+        attendance = Attendance.find(id)
+        attendance.update_attributes!(item)
+    end
+  end
+  
+    flash[:success] = "１ヶ月分の勤怠情報を更新しました。"
+    redirect_to user_url(date:params[:date])
+  rescue ActiveRecord::RecordInvalid
+    flash[:danger] = "無効な入力データがあった為、更新をキャンセルしました。"
+    redirect_to attendances_edit_one_month_user_url(date: params[:date])
   end
   
   private
