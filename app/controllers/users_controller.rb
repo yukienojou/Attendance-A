e332345d8992c263c7847f06c46fc300c58fe325
EC2 Default User <ec2-user@ip-172-31-26-135.ap-northeast-1.compute.internal>
2021-12-21 14:31:31 +0000

    ログイン

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -65,11 +65,11 @@ class UsersController < ApplicationController
   private
 
     def user_params
-      params.require(:user).permit(:name, :email, :department, :password, :password_confirmation)
+      params.require(:user).permit(:name, :email, :affiliation, :password, :password_confirmation)
     end
 
     def basic_info_params
-      params.require(:user).permit(:department, :basic_time, :work_time)
+      params.require(:user).permit(:affiliation, :basic_time, :work_time)
     end
     
      # システム管理権限所有かどうか判定します。
