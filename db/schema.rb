5f758eec1bae1e46e8dd3e093977e71e43ae7c5e
yukienojou <ec2-user@ip-172-31-26-135.ap-northeast-1.compute.internal>
2022-01-20 14:37:48 +0000

    拠点情報３

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20220119142108) do
+ActiveRecord::Schema.define(version: 20211213113759) do
 
   create_table "attendances", force: :cascade do |t|
     t.date "worked_on"
@@ -21,12 +21,6 @@ ActiveRecord::Schema.define(version: 20220119142108) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["user_id"], name: "index_attendances_on_user_id"
-  end
-
-  create_table "bases", force: :cascade do |t|
-    t.integer "base_number"
-    t.string "base_name"
-    t.text "infomation"
   end
 
   create_table "users", force: :cascade do |t|
