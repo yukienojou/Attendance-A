88f9823b6c59d110b0732873bca82d109aba363e
yukienojou <ec2-user@ip-172-31-26-135.ap-northeast-1.compute.internal>
2022-01-22 14:44:19 +0000

    拠点一覧４

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,6 @@ Rails.application.routes.draw do
   get    '/login', to: 'sessions#new'
   post   '/login', to: 'sessions#create'
   delete '/logout', to: 'sessions#destroy'
-  
   resources :bases
   resources :users do
      collection { post :import }
