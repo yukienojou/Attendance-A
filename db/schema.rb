fe7bfbd3b53aec4d272f037a4201da07cab36dd8
yukienojou <ec2-user@ip-172-31-26-135.ap-northeast-1.compute.internal>
2022-02-03 14:43:28 +0000

    rollback

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20220202140646) do
+ActiveRecord::Schema.define(version: 20220121140709) do
 
   create_table "attendances", force: :cascade do |t|
     t.date "worked_on"
