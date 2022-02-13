1bc9bce0d03fb7486af796bd4a53ff1f8472a8e9
yukienojou <ec2-user@ip-172-31-26-135.ap-northeast-1.compute.internal>
2022-02-13 14:17:51 +0000

    申請のお知らせ

---
+++ db/migrate/20220213120515_add_month_request_to_attendances.rb
@@ -0,0 +1,9 @@
+class AddMonthRequestToAttendances < ActiveRecord::Migration[5.1]
+   # 1ヶ月の勤怠承認
+  def change
+    # 承認申請月
+    add_column :attendances, :month_approval, :date
+   # モーダルの変更ボタン
+    add_column :attendances, :change_month, :boolean, default: false
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20220211004520) do
+ActiveRecord::Schema.define(version: 20220213120515) do
 
   create_table "attendances", force: :cascade do |t|
     t.date "worked_on"
@@ -34,6 +34,8 @@ ActiveRecord::Schema.define(version: 20220211004520) do
     t.string "overtime_work"
     t.boolean "change", default: false
     t.boolean "verification", default: false
+    t.date "month_approval"
+    t.boolean "change_month", default: false
     t.index ["user_id"], name: "index_attendances_on_user_id"
   end
 
