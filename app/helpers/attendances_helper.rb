4d32d155e8a7fdffd7f7e2b3df6c28daba9400a6
yukienojou <ec2-user@ip-172-31-26-135.ap-northeast-1.compute.internal>
2022-02-20 05:28:38 +0000

    勤怠申請

--- app/helpers/attendances_helper.rb
+++ app/helpers/attendances_helper.rb
@@ -1,46 +1,33 @@
 module AttendancesHelper
-  
-  def attendance_state(attendance)
+
+  def attendance_state_start(attendance)
+    # 受け取ったAttendanceオブジェクトが当日と一致するか評価します。
     if Date.current == attendance.worked_on
       return '出勤' if attendance.started_at.nil?
-      return '退勤' if attendance.started_at.present? && attendance.finished_at.nil?
-      
     end
-   false
-  end
-  def attendances_invalid?
-    attendances = true
-    attendances_params.each do |id, item|
-      if item[:started_at].blank? && item[:finished_at].blank?
-        next
-      elsif item[:started_at].blank? || item[:finished_at].blank?
-        attendances = false
-        break
-      elsif item[:started_at] > item[:finished_at]
-        attendances = false
-        break
-      end
-    end
-    return attendances
+    # どれにも当てはまらなかった場合はfalseを返します。
+    return false
   end
 
-  
-  
-  def working_times(start, finish) 
-    format("%.2f", (((finish - start) / 60) / 60.0))
-    
-  end
-  
-  def css_class(wo)
-    case $days_of_the_week[wo.wday]
-    when '土'
-      'text-primary'
-    when '日'
-      'text-danger'
+  def attendance_state_finish(attendance)
+    # 受け取ったAttendanceオブジェクトが当日と一致するか評価します。
+    if Date.current == attendance.worked_on
+      return '退勤' if attendance.started_at.present? && attendance.finished_at.nil?
     end
+    # どれにも当てはまらなかった場合はfalseを返します。
+    return false
   end
   
-    # 時間外時間
+  def working_times(start, finish)
+    format("%.2f", (((finish - start) / 60) / 60.0))
+  end
+  
+  def format_min(time)
+    format("%.2d",((time.strftime('%M').to_i / 15).round) * 15)
+  end 
+  
+   
+  # 時間外時間
   def overtime_worked_on(finish, end_time, tomorrow)    
     if tomorrow == true
       # finishとend_timeの時と分をそれぞれ計算し、差分を合わせるために、分側を60で割っている
--- db/migrate/20220210122506_add_overtime_to_attendances.rb
+++ db/migrate/20220210122506_add_overtime_to_attendances.rb
@@ -1,6 +1,6 @@
 class AddOvertimeToAttendances < ActiveRecord::Migration[5.1]
   def change
-    add_column :attendances, :overtime_finished_at, :string
+    add_column :attendances, :overtime_finished_at, :datetime
     add_column :attendances, :tomorrow, :boolean, default: false
     add_column :attendances, :overtime_work, :string
 
