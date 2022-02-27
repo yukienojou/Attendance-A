f4c58269560901f0e4a0bbcfadf55cdf659bd831
yukienojou <ec2-user@ip-172-31-26-135.ap-northeast-1.compute.internal>
2022-02-27 14:39:06 +0000

    勤怠ログ

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -27,6 +27,14 @@ class ApplicationController < ActionController::Base
     end
    end
   
+  def correct_user_b
+    @user = User.find(params[:user_id])
+    unless current_user?(@user)
+      flash[:danger] = "他者のページは閲覧できません"
+      redirect_to(root_url) 
+    end
+  end 
+  
   def admin_user
     redirect_to root_url unless current_user.admin?
   end
